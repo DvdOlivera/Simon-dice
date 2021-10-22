024bea45e6a675e45acee5443bd5d97c2a027eac
DvdOlivera <olivera.david.antonio@gmail.com>
2021-10-22 18:55:25 -0300

    upload
    upload

--- main.js
+++ main.js
@@ -1,3 +1,46 @@
+
+function sonidoClickAmarillo(){
+    const bleepAmarillo = new Audio();
+    bleepAmarillo.src="/sonido-amarillo.wav"
+    bleepAmarillo.currentTime = 0;
+    bleepAmarillo.play ();
+    
+};
+
+function sonidoClickAzul(){
+    const bleepAzul = new Audio();
+    bleepAzul.src="/sonido-azul.wav"
+    bleepAzul.currentTime = 0;
+    bleepAzul.play ();
+    
+};
+
+function sonidoClickRojo(){
+    const bleepRojo = new Audio();
+    bleepRojo.src="/sonido-rojo.wav"
+    bleepRojo.currentTime = 0;
+    bleepRojo.play ();
+    
+};
+
+function sonidoClickVerde(){
+    const bleepVerde = new Audio();
+    bleepVerde.src="/sonido-verde.wav"
+    bleepVerde.currentTime = 0;
+    bleepVerde.play ();
+    
+};
+
+function sonidoClickError(){
+    const bleepError = new Audio();
+    bleepError.src="/sonido-error.wav"
+    bleepError.currentTime = 0;
+    bleepError.play ();
+    
+}
+
+
+
 document.querySelectorAll(".area").forEach(element => {
     element.addEventListener("click",manejarMovimientoUsuario);
 });
@@ -18,6 +61,18 @@ function habilitarBoton(){
 }
 
 function mostrarMovimientoMaquina(numero) {
+    if (numero === 0){
+        sonidoClickVerde()
+    }
+    else if (numero === 1) {
+        sonidoClickAmarillo()
+    }
+    else if (numero === 2) {
+        sonidoClickRojo()
+    }
+    else if (numero === 3) {
+        sonidoClickAzul()
+    }
     document.querySelectorAll(".area")[numero].classList.remove("desactivo") 
     setTimeout(()=> {
         document.querySelectorAll(".area")[numero].classList.add("desactivo"); 
@@ -40,7 +95,7 @@ async function ejecutarMaquina(){
     movimientosMaquina.push(obtenerNumeroAlAzar(0,4));  
     for (let movimiento of movimientosMaquina){
     mostrarMovimientoMaquina(movimiento);
-    await retraso(1000); 
+    await retraso(700); 
     };
     habilitarClick();
 }
@@ -76,16 +131,16 @@ function guardarMovimientoUsuario(e){
 
 function compararMovimientos(){
     let error = false;
-    movimientosUsuario.forEach((movimiento,i) =>{
+    movimientosUsuario.forEach(async (movimiento,i) =>{
         if (movimiento === movimientosMaquina[i]){
        }else{
-            error = true;
-               setTimeout(() => {
-                finalizarPartida();
-                deshabilitarClick();
-                habilitarBoton()
-               }, 0);
-            };
+         error = true;  
+         deshabilitarClick();
+         sonidoClickError()
+         finalizarPartida();
+         await retraso(3000); 
+         habilitarBoton()
+        };
 
     });
 
@@ -95,14 +150,15 @@ function compararMovimientos(){
            partidas++
             actualizarContadorDePartidas(partidas)
             ejecutarMaquina();
-        }, 2000);
+        }, 1500);
     };
 };
 function actualizarContadorDePartidas(partidas = 0){
     document.querySelector("#contador-de-partidas").innerHTML = partidas
 }
 function finalizarPartida(){
-    alert("perdiste");
+    
+    
 };
 
 function borrarMovimientosMaquina(){
@@ -116,15 +172,3 @@ function obtenerNumeroAlAzar(min,max) {
     return Math.floor(Math.random() * (max - min)) + min;
   };
 
-
-function mostrarAlertaTurno(turno){
-
-    if(turno===true){
-        document.querySelector(".turno").innerHTML="es tu turno!"
-    }
-    if(turno===false){
-        document.querySelector(".turno").innerHTML=" turno de la maquina!"
-    }
-};
-
-//  transform: perspective( 700px ) rotateX( 45deg );
